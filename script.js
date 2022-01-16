9aedcd9a03033f24022384b485d9825e488e961b
AlexandreVe <94561326+AlexandreVe@users.noreply.github.com>
2022-01-16 16:37:49 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,26 @@
+/*let time = 3000,
+    currentImageIndex = 0
+    Image = Document.querySelectorAll("#slider img")
+    max = Image.length
+
+function nextImage () {
+    
+    Image[currentImageIndex]
+        .classList.remove("selected")
+    
+    currentImageIndex++
+    
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+    
+    Image[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start () {
+    setInterval ( () => {
+        nextImage ()
+    }, time)
+}
+
+window.addEventListener ("load", start)*/
